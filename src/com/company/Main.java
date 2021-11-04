c1a1376a272e96a752389f9a55bb5e3d0650c8c5
noorkhan3226 <noorafghan3226@gmail.com>
2021-11-03 20:14:19 -0400

    the blue ferrari car better then lambooo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         Habibi habibi=new Habibi();
-        
+        habibi.model="Ferrari";
+        habibi.color="blue";
+        habibi.year=1997;
+        habibi.tyres="4";
+
+        System.out.println(habibi.color);
+
     }
 }
