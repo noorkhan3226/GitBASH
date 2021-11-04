d7c2c409daddb48edfada05691be1e9d754c37ef
noorkhan3226 <noorafghan3226@gmail.com>
2021-11-03 20:55:10 -0400

    the damn car you asked

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Habibi habibi=new Habibi();
-        habibi.model="Ferrari";
-        habibi.color="blue";
-        habibi.year=1997;
-        habibi.tyres="4";
 
-        System.out.println(habibi.color);
+        Habibi habibi=new Habibi();
+
+        habibi.model="Lamboo";
+        habibi.color="Green";
+        habibi.tyres="6";
+        habibi.year=2001;
+
+        System.out.println(habibi.model);
+        
 
     }
 }
